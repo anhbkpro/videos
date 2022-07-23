01b193fe9c9c59389c39af60b4a7d579277b0d43
Anh Lai <laingocanh1990@gmail.com>
2022-07-23 22:47:31 +0700

    using repeated and enum data type of proto

--- gen/ruby/user/v1/user.pb.rb
+++ gen/ruby/user/v1/user.pb.rb
@@ -10,9 +10,20 @@ module User
     ::Protobuf::Optionable.inject(self) { ::Google::Protobuf::FileOptions }
 
     ##
+    # Enum Classes
+    #
+    class MaritalStatus < ::Protobuf::Enum
+      define :MARITAL_STATUS_UNSPECIFIED, 0
+      define :MARITAL_STATUS_SINGLE, 1
+      define :MARITAL_STATUS_MARRIED, 2
+    end
+
+
+    ##
     # Message Classes
     #
     class User < ::Protobuf::Message; end
+    class Address < ::Protobuf::Message; end
 
 
     ##
@@ -28,6 +39,14 @@ module User
       optional :string, :uuid, 1
       optional :string, :full_name, 2
       optional :int64, :birth_year, 3
+      optional :uint32, :salary, 4
+      repeated ::User::V1::Address, :addresses, 5
+      optional ::User::V1::MaritalStatus, :marital_status, 6
+    end
+
+    class Address
+      optional :string, :street, 1
+      optional :string, :city, 2
     end
 
   end
